dcd1c06c9a6d7dd344edf6831241bfd2e8f6eabf
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2021-09-30 09:38:52 +0200

    modif preneur

--- src/Component/Assujetti/LocatePreneur.js
+++ src/Component/Assujetti/LocatePreneur.js
@@ -20,6 +20,16 @@ const Where = () => {
 
       <div className="breadcrumbs">
         <p><img src={Check} alt="fleche grise"/>Le preneur est assujetti a la TVA</p>
+        <p><img src={Check} alt="fleche grise"/>La prestation entre dans les conditions citées précédemment :</p>
+        <div className='breadcrumbs_details' >
+          <p>Les prestations ne se rattachant pas à un bien immeuble.</p>
+          <p>Les transports de biens.</p>
+          <p>Les expertises et travaux portant sur des biens meubles corporels.</p>
+          <p>Les prestations immatérielles.</p>
+          <p>Les prestations de stockage accompagnées de prestations complexes.</p>
+          <p>Les prestations de services liées à des activités culturelles, artistiques, sportives, scientifiques, éducatives... <br />
+          Mais ne consistant pas à donner accès à des manifestations de ce type.</p>
+        </div>
       </div>
 
       <h2>Le preneur est il établi en France ?</h2>
